044bc5ffb699cf807ef0f169f7517bc3d179a851
Minh Khanh Pham <minh.pham@mobileappscompany.us>
2019-06-18 11:07:40 -0400

    final

--- app/src/main/java/com/example/week2test/Duplicates.java
+++ app/src/main/java/com/example/week2test/Duplicates.java
@@ -2,6 +2,7 @@ package com.example.week2test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -31,11 +32,30 @@ public class Duplicates {
     {
         char[][] testboard={{'a','b','c'},{'a','b','c'},{'a','b','d'}};
         Map<Character, ArrayList<Location>> mapofloaction=duplicate(testboard);
+        ArrayList<Location> final_list_location=new ArrayList<>();
         for (char k:mapofloaction.keySet()){
 
-            if ((mapofloaction.get(k).size()>1))System.out.println(mapofloaction.get(k).toString());
+            if ((mapofloaction.get(k).size()>1)){
+                for(Location l:mapofloaction.get(k)){
+                    final_list_location.add(l);
+                }
+            }
 
         }
+        Collections.sort(final_list_location);
+        StringBuilder str=new StringBuilder();
+        for(Location l:final_list_location){
+            str.append(l.toString()+"|");
+        }
+        if (str.length()>1){
+            str.delete(str.length()-1,str.length());
+        }
+        System.out.println(str.toString());
+
+
+
+
+
 
 
     }
@@ -43,7 +63,7 @@ public class Duplicates {
 
 }
 
-class Location{
+class Location implements Comparable{
     public Location(int x,int y){
         this.x=x;
         this.y=y;
@@ -66,4 +86,23 @@ class Location{
 
     public int x;
     public int y;
+
+    @Override
+    public int compareTo(Object o) {
+        if (this.getX()>((Location)o).getX()){
+            return 1;
+        }
+        else if (this.getX()<((Location)o).getX()){
+            return -1;
+        }
+        else if (this.getX()==((Location)o).getX()){
+            if (this.getY()>((Location)o).getY()){
+                return 1;
+            }
+            else if (this.getY()<((Location)o).getY())
+                return -1;
+
+        }
+        return 0;
+    }
 }
